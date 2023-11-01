7d000f05fa6ec6073d6e6e592613f3bbfdde6d70
stian <65687147+sti-and@users.noreply.github.com>
2023-11-01 17:27:21 +0100

    fixed navbar, added burger menu toggle, added js for menu, made navbar sticky

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,14 @@
 // JS only used for the burger menu
 // https://www.w3schools.com/js/
 
+// gets navbar-buttons (first element in returned array)
+const navbarbuttons = document.getElementsByClassName('navbar-buttons')[0]
+
+// gets navbar-burgericon (first element in returned array)
+const burgericon = document.getElementsByClassName('navbar-burgericon')[0]
+
+// adds event listener to the burger icon
+// when pressed it will toggle the navbar-buttons by running code in the css labeled with .active
+burgericon.addEventListener('click', () => {
+    navbarbuttons.classList.toggle('active')
+})
