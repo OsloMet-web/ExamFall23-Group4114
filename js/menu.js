d683c9607ef307770f487156f53fe5b846833da1
stian <65687147+sti-and@users.noreply.github.com>
2023-11-02 10:04:24 +0100

    changed class to id for js

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,10 @@
 // https://www.w3schools.com/js/
 
 // gets navbar-buttons (first element in returned array)
-const navbarbuttons = document.getElementsByClassName('navbar-buttons')[0]
+const navbarbuttons = document.getElementsByClassName("navbar-buttons")[0]
 
-// gets navbar-burgericon (first element in returned array)
-const burgericon = document.getElementsByClassName('navbar-burgericon')[0]
+// gets navbar-burgericon by its id
+const burgericon = document.getElementById("navbar-burgericon")
 
 // adds event listener to the burger icon
 // when pressed it will toggle the navbar-buttons by running code in the css labeled with .active
